0d883bb1229caa1777147d15075020f05cf9a744
biyolog1 <ulascantepe1979@gmail.com>
2018-12-03 14:44:43 +0300

    controller ve view degisiklikleri

---
+++ panel/application/controllers/Product.php
@@ -0,0 +1,15 @@
+<?php
+class Product extends  CI_Controller {
+    public $viewFolder ="";
+    public function __construct()
+    {
+        parent::__construct();
+        $this->viewFolder="product-v";
+    }
+    public function index(){
+        $viewData= new stdClass();
+        $viewData->viewFolder= $this->viewFolder;
+        $viewData->subViewFolder="list";
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index",$viewData);
+    }
+}
