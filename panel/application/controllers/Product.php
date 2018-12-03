5f8897bdb650c3b800a16d2530216a84d444b979
biyolog1 <ulascantepe1979@gmail.com>
2018-12-03 16:32:51 +0300

    product model and content

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -5,11 +5,20 @@ class Product extends  CI_Controller {
     {
         parent::__construct();
         $this->viewFolder="product-v";
+        $this->load->model("Product_model");
     }
     public function index(){
         $viewData= new stdClass();
+
+
+
+        /** Tabloadn Verilerin Getirilmesi*/
+        $items=$this->Product_model->get_all();
+        /** View'e gönderilecek değişkenlerin set edilmesi */
         $viewData->viewFolder= $this->viewFolder;
         $viewData->subViewFolder="list";
+
+        $viewData->items=$items;
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index",$viewData);
     }
 }
